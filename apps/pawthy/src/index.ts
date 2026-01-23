922ba3d35873262f74acc415aa2f1de262ec504f
Arafat Zahan <kuasha420@gmail.com>
2026-01-23 09:41:27 +0600

    refactor(pawthy): use import attributes for package.json

--- apps/pawthy/src/index.ts
+++ apps/pawthy/src/index.ts
@@ -1,18 +1,12 @@
 #!/usr/bin/env node
 import { Command } from "commander";
+import packageJson from '../package.json' with { type: 'json' };
 import { loginCommand } from './commands/login.js';
 import { initCommand } from './commands/init.js';
 import { pushCommand } from './commands/push.js';
 import { pullCommand } from './commands/pull.js';
 
 const program = new Command();
-
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8'));
 
 program
     .name("pawthy")
